8e820f784e3e8db3c082f1cb92d2df941b177abf
SSY <993721@naver.com>
2025-08-22 02:55:52 +0900

    이미지 수정

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -2,7 +2,7 @@ export const heroData = {
   name: "심성용 : PORTFOLIO",
   title: "Back-End Developer",
   description: "한국폴리텍IV대학 대전캠퍼스 스마트소프트웨어과 심성용입니다.",
-  profilePicture: "profile_image.png",
+  profilePicture: "profile_image_recent.png",
   skills: [
     "Spring",
     "Spring Security",
